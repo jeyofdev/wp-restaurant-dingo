fe7937d3cb48143a41a8f5c0379e7ae20e22c1db
jeyofdev <jgregoire.pro@gmail.com>
2020-06-16 14:50:34 +0200

    add the single post template

---
+++ web/app/themes/wpdingorestaurant/single-post.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * The single post template file
+ */
+
+use Timber\Post;
+use Timber\Timber;
+use jeyofdev\wp\dingo\restaurant\extending\Site;
+
+
+
+$context = Timber::context();
+$context["site"] = new Site();
+$context["post"] = new Post();
+
+$templates = "pages/single-post.twig";
+
+
+
+Timber::render($templates, $context);
