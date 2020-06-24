e5729a3a94588dc1abc427e90688429bb5132aca
jeyofdev <jgregoire.pro@gmail.com>
2020-06-24 15:41:25 +0200

    add the sections 'food menus', 'chefs', 'video' and 'testimonials' to front page

--- web/app/themes/wpdingorestaurant/front-page.php
+++ web/app/themes/wpdingorestaurant/front-page.php
@@ -11,6 +11,38 @@ use jeyofdev\wp\dingo\restaurant\extending\Site;
 
 $context = Timber::context();
 $context["site"] = new Site();
+$context["food_menus_page"] = Timber::get_post([
+    "post_type" => "page",
+    "name" => "menus"
+]);
+$context["food_menus"] = Timber::get_posts([
+    "post_type" => "food_menu",
+    "posts_per_page" => -1,
+    "order" => "DESC"
+]);
+$context["food_menus_terms"] = Timber::get_terms([
+    "post_type" => "food_menu",
+    "taxonomy" => "food_menu_type"
+]);
+
+$context["chefs"] = Timber::get_posts([
+    "post_type" => "chef",
+    "posts_per_page" => 3,
+    "order" => "DESC"
+]);
+$context["chefs_page"] = Timber::get_post([
+    "post_type" => "page",
+    "name" => "chefs"
+]);
+$context["testimonials"] = Timber::get_posts([
+    "post_type" => "testimonial",
+    "posts_per_page" => 10,
+    "order" => "DESC"
+]);
+$context["about_page"] = Timber::get_post([
+    "post_type" => "page",
+    "name" => "about"
+]);
 $context["blog"] = Timber::get_posts([
     "post_type" => "post",
     "post_per_page" => 3,
